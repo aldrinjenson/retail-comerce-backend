bcf702c8eaa8853d1da22d53560301418e406c83
Aldrin Jenson <aldrinjenson@gmail.com>
2021-08-08 23:43:44 +0530

    Refactoring

--- routes/companyRoutes.js
+++ routes/companyRoutes.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const { CompanyController } = require("../controllers");
 
 router.get("/", async (req, res) => {
-  const { company } = req.body;
-  res.send(await CompanyController.getCompany(company));
+  const { query } = req.body;
+  res.send(await CompanyController.getCompany(query));
 });
 
 router.post("/add", async (req, res) => {
